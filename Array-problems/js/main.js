8ef16ea9e75ac851cf66c83770b9d9aa08098bfa
ismael garcia <leamsigc@hotmail.com>
2017-04-24 23:49:33 -0400

    Esto es parte del curso de colt steel de Udemy

---
+++ Array-problems/js/main.js
@@ -0,0 +1,39 @@
+console.log("we are connected ");
+// ************** print reverse ************
+var arr = [1, 2, 3, 4, 5, 6];
+// function print reverse 
+    function  printReverse(arr) {
+        for (var i = arr.length -1; i >= 0 ; i--) {
+            console.log(arr[i]);
+        }
+    };
+// call the function 
+printReverse(arr);
+
+
+// *&&&&&   print equivalent &&&&&&&&&
+var x = [10,10,10,10];
+console.log(x);
+// function equals
+    function Equal(x) {
+        var first= x[0];
+        for (var i = 1; i < x.length; i++) {
+            if(x[i] !== first){
+                return false;
+            }
+        }
+        return true;
+    };
+// call function 
+    Equal(x);
+//************ sum all var in the array     ********** */
+var num = [10,10,10,20];
+// declare the function sum 
+    function sum ( num){
+        var total = 0;
+        num.forEach(function(element) {
+            total+=element;
+        }, this);
+        return total;
+    }
+    sum(num);
