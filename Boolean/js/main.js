e02680498165825863bfa021ff7fd6ba510400c3
ismael garcia <leamsigc@hotmail.com>
2017-05-11 09:29:55 -0400

    Boolean data type

---
+++ Boolean/js/main.js
@@ -0,0 +1,22 @@
+console.log("we are connected");
+//only two possibles values
+console.log(5<6);
+console.log("===== this will be true==========");
+// to check if the are equal
+console.log( 5 == 5);//if the are the same type of data
+console.log("===== this will be true==========");
+// All real value will return true
+console.log(Boolean(100));
+console.log(Boolean(3.14));
+console.log(Boolean(-5));
+console.log(Boolean(2+3+5));
+console.log(100 < 101);
+console.log(Boolean("Hello"));
+// All no real values will return false
+console.log("===== this will be False ==========");
+var x=0;
+console.log(Boolean(x));
+console.log(Boolean(""));
+console.log(Boolean(null));
+var y = 10 * "h";
+console.log(Boolean(y));
