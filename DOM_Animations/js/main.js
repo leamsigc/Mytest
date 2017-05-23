1d786d0de562d53e8e2c87681b9a9fe16c410385
ismael garcia <leamsigc@hotmail.com>
2017-05-23 18:00:02 -0400

    change the tab for spaces

--- DOM_Animations/js/main.js
+++ DOM_Animations/js/main.js
@@ -1,15 +1,15 @@
 var btn = document.getElementById('button');
 btn.addEventListener('click',moveOn);
 function moveOn(){
-    var small = document.querySelector('.animate');
-    var pos = 0 ;
-    var myInterval = setInterval(frame , 2);
-    function frame (){
-        if(pos == 250){
-            clearInterval(myInterval);
-        }
-        pos++;
-        small.style.top = pos + "px";
-        small.style.left= pos + "px";
-    }
+	var small = document.querySelector('.animate');
+	var pos = 0 ;
+	var myInterval = setInterval(frame , 2);
+	function frame (){
+		if(pos == 250){
+			clearInterval(myInterval);
+		}
+		pos++;
+		small.style.top = pos + "px";
+		small.style.left= pos + "px";
+	}
 }
